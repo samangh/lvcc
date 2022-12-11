ee21e1f4134639edfcf4d8432dd93bbcbd0544ad
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2022-12-11 11:11:53 +0000

    Use pre-processor macro to define arr1D_*

--- lvcc/include/lvcc/types_arr1D.h
+++ lvcc/include/lvcc/types_arr1D.h
@@ -2,69 +2,42 @@
 
 #include <extcode.h>
 
+#define GEN_1D_ARRAY(T)         \
+    typedef struct {            \
+        int32_t dimSize;        \
+        T elt[1];               \
+    } arr1D_##T;                \
+    typedef arr1D_##T** arr1DH_##T;
+
+#define GEN_nD_ARRAY(N,T)       \
+    typedef struct {            \
+        int32_t dimSize[N];     \
+        T elt[1];               \
+    } arr_##N##T;               \
+
+#define GEN_ARRAY(T)            \
+    GEN_1D_ARRAY(T)             \
+    GEN_nD_ARRAY(2,T)           \
+    GEN_nD_ARRAY(3,T)           \
+    GEN_nD_ARRAY(4,T)           \
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 #include <lv_prolog.h>
 
-typedef struct {
-    int8_t dimSize;
-    int8 elt[1];
-} arr1D_int8;
-typedef arr1D_int8** arr1DH_int8;
+GEN_1D_ARRAY(int8)
+GEN_1D_ARRAY(int16)
+GEN_1D_ARRAY(int32)
+GEN_1D_ARRAY(int64)
 
-typedef struct {
-    int16_t dimSize;
-    int16 elt[1];
-} arr1D_int16;
-typedef arr1D_int16** arr1DH_int16;
+GEN_1D_ARRAY(uInt8)
+GEN_1D_ARRAY(uInt16)
+GEN_1D_ARRAY(uInt32)
+GEN_1D_ARRAY(uInt64)
 
-typedef struct {
-    int32_t dimSize;
-    int32 elt[1];
-} arr1D_int32;
-typedef arr1D_int32** arr1DH_int32;
-
-typedef struct {
-    int32_t dimSize;
-    int64 elt[1];
-} arr1D_int64;
-typedef arr1D_int64** arr1DH_int64;
-
-typedef struct {
-    int32_t dimSize;
-    uInt8 elt[1];
-} arr1D_uint8;
-typedef arr1D_uint8** arr1DH_uint8;
-
-typedef struct {
-    int32_t dimSize;
-    uInt16 elt[1];
-} arr1D_uint16;
-typedef arr1D_uint16** arr1DH_uint16;
-
-typedef struct {
-    int32_t dimSize;
-    uInt32 elt[1];
-} arr1D_uint32;
-typedef arr1D_uint32** arr1DH_uint32;
-
-typedef struct {
-    int32_t dimSize;
-    uInt64 elt[1];
-} arr1D_uint64;
-typedef arr1D_uint64** arr1DH_uint64;
-
-typedef struct {
-    int32_t dimSize;
-    double elt[1];
-} arr1D_double;
-typedef arr1D_double** arr1DH_double;
-
-typedef struct {
-    int32_t dimSize;
-    LVBoolean elt[1];
-} arr1D_bool, **arr1DH_bool;
+GEN_1D_ARRAY(double)
+GEN_1D_ARRAY(bool)
 
 #include <lv_epilog.h>
 #ifdef __cplusplus
